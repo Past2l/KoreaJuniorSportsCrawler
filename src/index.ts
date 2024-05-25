92022adf76a98e138a080c2eea67fc0eed856837
Dohyeon Yun <yundohyun050121@gmail.com>
2024-05-25 14:07:09 +0900

    add: `node-cron` package

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import 'dotenv/config';
 import { Sports } from './module';
+import cron from 'node-cron';
 
 async function bootstrap() {
-  const date = new Date();
-  await Sports.save(
-    process.env.OUTPUT || './output',
-    await Sports.init(),
-    date,
-  );
+  cron.schedule('0 * * * *', async () => {
+    const date = new Date();
+    await Sports.save(
+      process.env.OUTPUT || './output',
+      await Sports.init(),
+      date,
+    );
+  });
 }
 bootstrap();
