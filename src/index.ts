bdbc80fd0303b03b492af29e5218510a8575498c
Dohyeon Yun <yundohyun050121@gmail.com>
2024-05-25 15:19:40 +0900

    edit: `index.ts` crontab schedule

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Sports } from './module';
 import cron from 'node-cron';
 
 async function bootstrap() {
-  cron.schedule('0 * * * *', async () => {
+  cron.schedule('*/15 * * * *', async () => {
     const date = new Date();
     await Sports.save(
       process.env.OUTPUT || './output',
